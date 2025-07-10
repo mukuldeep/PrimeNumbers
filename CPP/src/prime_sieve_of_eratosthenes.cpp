606ef8f4385db99416bba9d3e2434c00524f916f
mukuldeep maiti <mukul0018@gmail.com>
2025-07-10 21:27:52 +0530

    resolves #2 c++ implementation for prime number list by sieve

---
+++ CPP/src/prime_sieve_of_eratosthenes.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void sieveOfEratosthenes(int n) {
+    vector<bool> isPrime(n + 1, true); // Index i represents if i is prime
+    isPrime[0] = isPrime[1] = false; // 0 and 1 are not primes
+
+    for (int i = 2; i * i <= n; ++i) {
+        if (isPrime[i]) {
+            // Mark all multiples of i as non-prime
+            for (int j = i * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+
+    // Print all primes
+    cout << "Prime numbers up to " << n << " are:\n";
+    for (int i = 2; i <= n; ++i) {
+        if (isPrime[i])
+            cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter upper limit: ";
+    cin >> n;
+
+    sieveOfEratosthenes(n);
+    return 0;
+}
+
+
+/**
+ *
+ * Time and Space Complexity:
+ * Time: O(n log log n)
+ * Space: O(n)
+ *
+ */
